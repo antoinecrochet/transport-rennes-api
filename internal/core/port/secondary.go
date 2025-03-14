ea32a910431a780bcf397df694961b87273264ac
Antoine Crochet <acrochet95@gmail.com>
2025-03-14 19:56:54 +0100

    test: search service with port mocked

--- internal/core/port/secondary.go
+++ internal/core/port/secondary.go
@@ -2,6 +2,8 @@ package port
 
 import "github.com/antoinecrochet/transport-rennes-api/internal/core/model"
 
+//go:generate mockgen -source=secondary.go -destination=mock/secondary.go
+
 type DataProviderPort interface {
 	SearchUpcomingPublicTransports(search model.Search) (model.SearchResult, error)
 }
